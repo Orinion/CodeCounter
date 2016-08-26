cb7b2bde8e35ac2950de0dbc88bd8430b005964e
Orinion <orinion77@gmail.com>
2016-08-26 18:26:27 +0200

    first commit

---
+++ CodeCounter/src/de/CodeCounter/LineCounter.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.CodeCounter;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author Jakob
+ */
+public class LineCounter {
+    private File rootFile;
+    private int lines=0;
+    private int directories=0;
+    private int sourceFiles=0;
+    public LineCounter(File file) {
+        this.rootFile = file;
+        this.calc();
+    }
+    private void getLines(File file)
+    {
+        if(!file.exists())
+            return;
+        if(file.isDirectory()){
+            directories++;
+            for(File child: file.listFiles())
+                getLines(child);
+        }
+        else
+        {
+            sourceFiles++;
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(file));
+                while(reader.readLine()!=null)
+                    lines++;
+            } catch (IOException ex) {}
+        }
+    }
+    private void calc()
+    {
+        getLines(rootFile);
+    }
+  
+    public String getFilePath() {
+        return rootFile.getPath();
+    }
+
+    public int getLines() {
+        return lines;
+    }
+
+    public int getDirectories() {
+        return directories;
+    }
+
+    public int getSourceFiles() {
+        return sourceFiles;
+    }
+    
+}
